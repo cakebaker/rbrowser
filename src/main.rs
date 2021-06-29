6a81ae3480e5c85d3c25364eb0f4c07dc8b86cb2
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2021-06-29 10:23:36 +0200

    Read url from command line

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+#![warn(clippy::all, clippy::nursery, clippy::pedantic)]
+
+use std::env;
+
+fn main() {
+    let mut args = env::args();
+    args.next();
+
+    let url = match args.next() {
+        Some(arg) if arg.starts_with("http://") => Url::new(&arg),
+        None => {
+            println!("Usage: rbrowser <URL>");
+            return;
+        }
+        _ => {
+            println!("URL must start with 'http://'");
+            return;
+        }
+    };
+
+    println!("{:?}", url);
+}
+
+#[derive(Debug)]
+struct Url {
+    host: String,
+    path: String,
+}
+
+impl Url {
+    pub fn new(url: &str) -> Self {
+        let url = &url["http://".len()..];
+        let mut elements = url.splitn(2, |e| e == '/');
+
+        let host = elements.next().unwrap().to_owned();
+        let path = match elements.next() {
+            Some(path) => "/".to_owned() + path,
+            None => "/".to_owned(),
+        };
+
+        Self { host, path }
+    }
+}
