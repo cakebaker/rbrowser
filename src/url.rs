13247dfcd08c0b84044a7951e325e18384ffbd76
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2021-07-04 14:59:39 +0200

    Support custom ports in urls

--- src/url.rs
+++ src/url.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
 pub enum Scheme {
@@ -33,14 +34,23 @@ impl Url {
             _ => return Err("Unknown scheme, must be http or https"),
         };
 
-        let port = match scheme {
+        let mut port = match scheme {
             Scheme::Http => 80,
             Scheme::Https => 443,
         };
 
         let mut host_and_path = scheme_and_rest[1].splitn(2, '/');
+        let mut host = host_and_path.next().unwrap().to_owned();
 
-        let host = host_and_path.next().unwrap().to_owned();
+        if host.contains(':') {
+            let mut host_and_port = host.splitn(2, ':');
+            // XXX assignment to 'host' not possible here because it's borrowed, hence using a temp
+            // var
+            let temp_host = host_and_port.next().unwrap().to_owned();
+            port = FromStr::from_str(host_and_port.next().unwrap()).unwrap();
+            host = temp_host;
+        }
+
         let path = match host_and_path.next() {
             Some(path) => "/".to_owned() + path,
             None => "/".to_owned(),
@@ -74,6 +84,14 @@ mod tests {
         assert_eq!(Scheme::Https, url.scheme);
         assert_eq!("example.org", url.host);
         assert_eq!(443, url.port);
+        assert_eq!("/path", url.path);
+    }
+
+    #[test]
+    fn new_url_with_custom_port() {
+        let url = Url::new("http://example.org:8080/path").unwrap();
+        assert_eq!("example.org", url.host);
+        assert_eq!(8080, url.port);
         assert_eq!("/path", url.path);
     }
 
