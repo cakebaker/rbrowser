93155904da00c0ba29c91fe3dcd570cc9d5b1d9d
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2021-07-06 16:53:10 +0200

    Only show text between body tags

--- src/browser.rs
+++ src/browser.rs
@@ -54,16 +54,78 @@ impl Browser {
         Ok((headers, body))
     }
 
-    pub fn show(body: &str) {
+    pub fn show(s: &str) {
+        let body = Self::get_body(s);
+        println!("{}", Self::remove_tags(body));
+    }
+
+    // Returns either the input string if there is no body tag, or the content between the body
+    // tags. The closing body tag is optional.
+    fn get_body(s: &str) -> &str {
+        let mut start_pos = 0;
+        let mut end_pos = s.len();
+
+        if let Some(pos) = s.find("<body>") {
+            start_pos = pos + "<body>".len();
+
+            if let Some(pos) = s.find("</body>") {
+                end_pos = pos;
+            }
+        }
+
+        &s[start_pos..end_pos]
+    }
+
+    fn remove_tags(s: &str) -> String {
+        let mut result = String::from("");
         let mut in_angle = false;
 
-        for c in body.chars() {
+        for c in s.chars() {
             match c {
                 '<' => in_angle = true,
                 '>' => in_angle = false,
-                _ if !in_angle => print!("{}", c),
+                _ if !in_angle => result.push(c),
                 _ => {}
             }
         }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_body() {
+        let result = Browser::get_body("start<body>text</body>end");
+        assert_eq!("text", result);
+    }
+
+    #[test]
+    fn get_body_from_empty_string() {
+        assert_eq!("", Browser::get_body(""));
+    }
+
+    #[test]
+    fn get_body_from_string_without_body_tags() {
+        assert_eq!("test", Browser::get_body("test"));
+    }
+
+    #[test]
+    fn get_body_from_string_without_closed_body() {
+        let result = Browser::get_body("start<body>text");
+        assert_eq!("text", result);
+    }
+
+    #[test]
+    fn remove_tags() {
+        assert_eq!("test", Browser::remove_tags("<b>test</b>"));
+    }
+
+    #[test]
+    fn remove_tags_from_empty_string() {
+        assert_eq!("", Browser::remove_tags(""));
     }
 }
